63d10ae9d01f4d0ad6bd6bcded537e77d5d75313
Stannis Rupert <srupert@student.21-school.ru>
2022-01-30 01:40:38 +0300

    checkpoint

--- get_next_line.c
+++ get_next_line.c
@@ -2,3 +2,45 @@
 // Created by srupert on 1/30/22.
 //
 
+#include "get_next_line.h"
+
+char *gnl_read(char *remainder, int fd, ssize_t *ret);
+
+char *gnl_slice(char *remainder, char **result);
+
+char    *get_next_line(int fd)
+{
+	static char *remainder;
+	char *result;
+	char *tmp;
+	ssize_t ret;
+
+	result = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (result == NULL)
+	{
+		remainder = gnl_read(remainder, fd, &ret);
+		if (ret < 0)
+			return (NULL);
+		remainder = gnl_slice(remainder, &result);
+	}
+	return (result);
+}
+
+char *gnl_slice(char *remainder, char **result) {
+
+}
+
+char * gnl_read(char *remainder, int fd, ssize_t *ret) {
+	char *buf[BUFFER_SIZE + 1];
+	char *new_rem;
+
+	ret = read(fd, buf, BUFFER_SIZE);
+	if (ret < 0)
+		return (ret);
+	new
+
+
+	return 0;
+}
--- get_next_line.h
+++ get_next_line.h
@@ -7,7 +7,7 @@
 
 #include <malloc.h>
 #include <unistd.h>
-//#include "libft.h"
+#include "libft.h"
 
 #define BS 0
 #define BM 1
@@ -18,8 +18,5 @@
 #define OT 6
 
 char    *get_next_line(int fd);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
-char	*gnl_strdup(const char *s1);
 
 #endif //GNL_GET_NEXT_LINE_H
