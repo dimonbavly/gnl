f41ea862f16b76329d7b993f004d174420369b73
Stannis Rupert <srupert@student.21-school.ru>
2022-01-30 09:43:23 +0300

    norme fix

--- get_next_line.h
+++ get_next_line.h
@@ -1,24 +1,23 @@
-//
-// Created by srupert on 1/28/22.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srupert <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 09:43:02 by srupert           #+#    #+#             */
+/*   Updated: 2022/01/30 09:43:06 by srupert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef GNL_GET_NEXT_LINE_H
-#define GNL_GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <malloc.h>
-#include <unistd.h>
-#include "libft.h"
+# include <malloc.h>
+# include <unistd.h>
 
-#define BS 0
-#define BM 1
-#define BE 2
-#define RS 3
-#define RM 4
-#define RE 5
-#define OT 6
-
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
 
-#endif //GNL_GET_NEXT_LINE_H
+#endif //GET_NEXT_LINE_H
