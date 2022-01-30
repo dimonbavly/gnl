5eb545795141490c1a5c8a203df92baa1eca0603
Stannis Rupert <srupert@student.21-school.ru>
2022-01-30 09:44:45 +0300

    norme fix

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,7 +1,17 @@
-//
-// Created by srupert on 1/30/22.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srupert <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 09:44:21 by srupert           #+#    #+#             */
+/*   Updated: 2022/01/30 09:44:25 by srupert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+
 size_t	ft_strlen(const char *s)
 {
 	size_t	cnt;
@@ -12,15 +22,17 @@ size_t	ft_strlen(const char *s)
 	return (cnt);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n) {
-	char *d;
-	char *s;
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
 
 	if (dest == NULL && src == NULL)
 		return (dest);
 	d = (char *) dest;
 	s = (char *) src;
-	while (n) {
+	while (n)
+	{
 		*d = *s;
 		d++;
 		s++;
@@ -28,4 +40,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n) {
 	}
 	return (dest);
 }
-
