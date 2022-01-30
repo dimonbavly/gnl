bcd618772495e3d9e7d1bad2fa6247e77cdef3ed
Stannis Rupert <srupert@student.21-school.ru>
2022-01-30 09:40:53 +0300

    norme fix

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,34 @@
-//
-// Created by srupert on 1/30/22.
-//01234567890123456789012345678901234567890
-//0x604000000010 "0123456789012345678901234567890123456789\n0"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srupert <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 09:40:36 by srupert           #+#    #+#             */
+/*   Updated: 2022/01/30 09:40:39 by srupert          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char *gnl_read(char *remainder, int fd, ssize_t *ret);
+char	*gnl_read(char *remainder, int fd, ssize_t *ret);
 
-char *gnl_slice(char *remainder, char **result);
+char	*gnl_slice(char *remainder, char **result);
 
-char *gnl_chr(char *s, char c);
+char	*gnl_chr(char *s, char c);
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *remainder;
-	char *result;
-	char *tmp;
-	ssize_t ret;
+	static char	*remainder;
+	char		*result;
+	char		*tmp;
+	ssize_t		ret;
 
 	result = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while(1)
+	while (1)
 	{
 		if (remainder)
 			if (gnl_chr(remainder, '\n'))
@@ -39,32 +47,33 @@ char    *get_next_line(int fd)
 	return (result);
 }
 
-char *gnl_slice(char *remainder, char **result) {
-	char *new_rem;
-	size_t i;
+char	*gnl_slice(char *remainder, char **result)
+{
+	char	*new_rem;
+	size_t	i;
 
 	i = 0;
 	new_rem = NULL;
 	while (remainder && remainder[i] != '\n' && remainder[i] != '\0')
 		i++;
-	if (i > 0 ||  (remainder != 0 && remainder[0] == '\n')) //fix this
+	if (i > 0 || (remainder != 0 && remainder[0] == '\n'))
 		*result = malloc(sizeof(char) * (i + 1 + (remainder[i] == '\n')));
 	if (ft_strlen(&(remainder[i])) > 1)
-		new_rem = malloc(sizeof (char) *  (ft_strlen(&remainder[i]) + 1));
-	//	new_rem = malloc(sizeof (char) *  (ft_strlen(&remainder[i]) + 1));
+		new_rem = malloc(sizeof (char) * (ft_strlen(&remainder[i]) + 1));
 	if (*result == NULL || (new_rem == NULL && ft_strlen(&remainder[i]) > 1))
 		return (NULL);
-	memcpy(*result, remainder,  i + 1);
+	memcpy(*result, remainder, i + 1);
 	if (remainder[i] == '\n')
-		(*result)[i + 1] = '\0';//debug
+		(*result)[i + 1] = '\0';
 	if (new_rem)
 		memcpy(new_rem, &(remainder[i + 1]), ft_strlen(&(remainder[i + 1])));
 	return (new_rem);
 }
 
-char *gnl_read(char *remainder, int fd, ssize_t *ret) {
-	char buf[BUFFER_SIZE + 1];
-	char *new_rem;
+char	*gnl_read(char *remainder, int fd, ssize_t *ret)
+{
+	char	buf[BUFFER_SIZE + 1];
+	char	*new_rem;
 
 	*ret = read(fd, buf, BUFFER_SIZE);
 	if (*ret < 0 || (remainder == NULL && *ret == 0))
@@ -79,15 +88,14 @@ char *gnl_read(char *remainder, int fd, ssize_t *ret) {
 	return (new_rem);
 }
 
-
-char *gnl_chr(char *s, char c)
+char	*gnl_chr(char *s, char c)
 {
-	int i;
+	int	i;
 
 	i = -1;
 	if (s)
-		while(s[++i])
+		while (s[++i])
 			if (s[i] == c)
-				return(&s[i]);
+				return (&s[i]);
 	return (NULL);
 }
