290942b2c939a140afed0e4f90cf61e2a8acb537
Stannis Rupert <srupert@student.21-school.ru>
2022-01-30 10:17:39 +0300

    fix file

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -31,45 +31,12 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		return (dest);
 	d = (char *) dest;
 	s = (char *) src;
-<<<<<<< HEAD
-	while (n) {
-=======
 	while (n)
 	{
->>>>>>> ddd
 		*d = *s;
 		d++;
 		s++;
 		n--;
 	}
 	return (dest);
-<<<<<<< HEAD
-
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	cnt;
-
-	cnt = 0;
-	if (s)
-		while (s[cnt])
-			cnt++;
-	return (cnt);
-}
-
-char	*gnl_strdup(const char *s1)
-{
-	char	*res;
-	size_t	s1_len;
-
-	s1_len = ft_strlen(s1);
-	res = malloc(sizeof(*res) * s1_len + 1);
-	if (!res)
-		return (NULL);
-	else
-		ft_memcpy(res, s1, s1_len);
-	return (res);
-=======
->>>>>>> ddd
 }
